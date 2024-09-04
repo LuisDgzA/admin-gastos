50a2570cddee88d4cfe673bd3a4c7f6cd7f29537
LuisDgzA <luisdgza96@gmail.com>
2024-09-04 16:24:22 -0600

    add files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    // import Filtro from './components/Filtro.vue'
+</script>
+
+<template>
+    <div>
+        <header>
+            <h1>Planificador de gatos</h1>
+        </header>
+
+        <!-- <Filtro></Filtro> -->
+    </div>
+</template>
+
+<style>
+   
+</style>
