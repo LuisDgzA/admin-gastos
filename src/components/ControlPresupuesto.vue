6fe39eaad9a5f968ce05ef2863de4821339345bf
LuisDgzA <luisdgza96@gmail.com>
2024-09-11 06:14:06 -0600

    animation on modal add presupuesto

---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,110 @@
+<script setup>
+    import imagen from '../assets/img/grafico.jpg';
+
+    import { formatearCantidad } from '../helpers';
+
+    const props = defineProps({
+        presupuesto:{
+            type: Number,
+            required: true
+        },
+        disponible:{
+            type: Number,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+    <div class="dos-columnas">
+        <div class="contenedor-grafico">
+            <img
+                :src="imagen"
+            />
+        </div>
+        <div class="contenedor-presupuesto">
+            <button class="reset-app">
+                Resetear app
+            </button>
+            <p>
+                <span>Presupuesto:</span>
+                {{ formatearCantidad(presupuesto) }}
+            </p>
+            <p>
+                <span>Disponible:</span>
+                {{ formatearCantidad(disponible) }}
+            </p>
+            <p>
+                <span>Gastado:</span>
+                $0
+            </p>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .dos-columnas{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .dos-columnas > :first-child{
+        margin-bottom: 3rem;
+    }
+
+    .contenedor-presupuesto{
+        width: 100%;
+        p{
+            font-size: 2.4rem;
+            text-align: center;
+            color: var(--gris-oscuro);
+        }
+        span{
+            font-weight: 900;
+            color: var(--azul);
+        }
+    }
+
+    .reset-app{
+        background-color: #db2777;
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        color: var(--blanco);
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 1rem;
+        transition-property: background-color;
+        transition-duration: 0.3s;
+
+        &:hover{
+            cursor: pointer;
+            background-color: #bb1a62;
+
+        }
+    }
+
+    @media (width >= 768px) {
+        .dos-columnas{
+            flex-direction: row;
+            gap: 4rem;
+            align-items: center;
+        }
+         .dos-columnas > :first-child{
+            margin-bottom: 0rem;
+        }
+        .contenedor-presupuesto{
+            p{
+                /* font-size: 2.4rem; */
+                text-align: left;
+            }
+            span{
+
+            }
+        }
+    }
+    
+    
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,156 @@
+<script setup>
+    import cerrarModal from '../assets/img/cerrar.svg'
+
+    const emit = defineEmits(['ocultar-modal'])
+
+    const props = defineProps({
+        modal:{
+            type: Object,
+            required: true
+        },
+        nombre:{
+            type: String,
+            required: true
+        },
+        cantidad:{
+            type: [String, Number],
+            required: true
+        },
+        categoria:{
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div class="modal">
+        <div class="cerrar-modal">
+            <img
+                :src="cerrarModal"
+                @click="$emit('ocultar-modal')"
+            />
+        </div>
+        <div
+            class="contenedor contenedor-formulario"
+            :class="[modal.animar ? 'animar' : 'cerrar']"
+            >
+            <form class="nuevo-gasto">
+                <legend>Añadir gasto</legend>
+                <div class="campo">
+                    <label for="nombre">Nombre gasto:</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        placeholder="Añade el nombre del gasto"
+                        :value="nombre"
+                    />
+                </div>
+                <div class="campo">
+                    <label for="cantidad">Cantidad:</label>
+                    <input
+                        type="number"
+                        id="cantidad"
+                        placeholder="Añade la cantidad del gasto"
+                        :value="cantidad"
+                    />
+                </div>
+                <div class="campo">
+                    <label for="categoria">Categoría:</label>
+                    <select name=""
+                        id="categoria"
+                        :value="categoria"
+                        >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="varios">Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+                <input
+                    type="submit"
+                    value="Añadir gasto"
+                />
+            </form>
+        </div>
+    </div>
+</template>
+
+
+
+<style>
+    .modal{
+        position: absolute;
+        background-color: rgb(0 0 0 / 0.9);
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+
+    .contenedor-formulario{
+        transition: all 0.3s ease-in;
+        opacity: 0;
+
+        &.animar{
+            opacity: 1;
+        }
+
+        &.cerrar{
+            opacity: 0;
+        }
+    }
+
+    .cerrar-modal{
+        position: absolute;
+        right: 3rem;
+        top: 3rem;
+        cursor: pointer;
+        img{
+            width: 3rem;
+        }
+    }
+
+    .nuevo-gasto{
+        margin: 10rem auto 0 auto;
+        display: grid;
+        gap: 2rem;
+
+        input,select{
+            background-color: var(--gris-claro);
+            border-radius: 1rem;
+            padding: 1rem;
+            border: none;
+            font-size: 2.2rem;
+        }
+
+        label{
+            color: var(--blanco);
+            font-size: 3rem;
+        }
+
+        input[type="submit"]{
+            background-color: var(--azul);
+            color: var(--blanco);
+            font-weight: 700;
+            cursor: pointer;
+        }
+
+        legend{
+            text-align: center;
+            color: var(--blanco);
+            font-size: 3rem;
+            font-weight: 700;
+        }
+    }
+
+    .campo{
+        display: grid;
+        gap: 2rem;
+    }
+
+</style>
